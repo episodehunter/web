ebd2eb2dcbbbeeeff6e799b266331e6928a75642
Viktor Eriksson <me@viktoreriksson.se>
2018-03-03 08:15:38 +0100

    webpack.prod typo

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const common = require('./webpack.common.js')
+const common = require('./webpack.base.js')
 
 module.exports = merge(common, {
   plugins: [new UglifyJSPlugin()]
