4ca393004065e2fb7d29af7ea06b129c97a7f786
Viktor Eriksson <me@viktoreriksson.se>
2018-04-08 15:33:47 +0200

    💄  WIP: responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,13 +4,16 @@ import styled from 'styled-components'
 import { alabaster, gossamer, melrose } from '../utils/colors'
 import { inject, observer } from 'mobx-react'
 import { UserStore } from '../store/user'
+import { media } from '../styles/media-queries'
+import { observable, action } from 'mobx'
+import { Hamburger } from './hamburger'
 
 type Props = {
   user?: UserStore
 }
 
-const NavbarComponent = ({ user }: Props) => (
-  <Wrapper>
+const NavItems = ({ user }: Props) => (
+  <>
     <Item exact to="/">
       Upcoming
     </Item>
@@ -21,10 +24,54 @@ const NavbarComponent = ({ user }: Props) => (
       onClick={() => user!.logout()}
       src={user!.picture || 'https://episodehunter.tv/img/logga.png'}
     />
-  </Wrapper>
+  </>
 )
 
+class NavbarComponent extends React.Component<Props> {
+  @observable open: boolean = false
+
+  @action
+  toggle = () => {
+    this.open = !this.open
+  }
+
+  render() {
+    const { user } = this.props
+    return (
+      <>
+        <Wide className="wide">
+          <NavItems user={user} />
+        </Wide>
+        <Narrow className="narrow">
+          <Hamburger open={this.open} onToggle={this.toggle} />
+          {this.open && <NavItems user={user} />}
+        </Narrow>
+      </>
+    )
+  }
+}
+
 export const Navbar = inject('user')(observer(NavbarComponent))
+
+const Wide = styled.div`
+  justify-content: flex-end;
+  ${media.giant`display: flex;`};
+  ${media.desktop`display: flex;`};
+  ${media.tablet`display: flex;`};
+  display: none;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 1;
+`
+
+const Narrow = styled.div`
+  ${media.giant`display: none;`};
+  ${media.desktop`display: none;`};
+  ${media.tablet`display: none;`};
+  display: block;
+  margin: 20px;
+`
 
 const Image = styled.img.attrs({
   src: (props: { src?: string }) => props.src
@@ -46,10 +93,11 @@ const Item = styled(NavLink).attrs({
   font-family: 'Lato', sans-serif;
   text-transform: uppercase;
   text-decoration: none;
-  font-size: 14px;
   margin-right: 30px;
+  height: 25px;
   cursor: pointer;
   letter-spacing: 1.5px;
+  box-sizing: border-box;
   &:hover {
     border-bottom: 2px solid ${melrose};
   }
@@ -57,13 +105,8 @@ const Item = styled(NavLink).attrs({
     border-bottom: 2px solid ${gossamer};
   }
   margin-top: 20px;
-`
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  position: absolute;
-  top: 0;
-  right: 0;
-  z-index: 1;
+  ${media.giant`font-size: 14px;`};
+  ${media.desktop`font-size: 13px;`};
+  ${media.tablet`font-size: 12px;`};
+  font-size: 11px;
 `
