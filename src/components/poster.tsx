fd0104fcc22a1e9edeecb5f4cf55a5c2d002730d
tjoskar <kontakta@oskarkarlsson.nu>
2018-04-15 14:29:30 +0200

    ➕  Include lazy load for show posters

--- src/components/poster.tsx
+++ src/components/poster.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import { LazyLoadImage } from '@tjoskar/react-lazyload-img'
 import { images } from '../images.config'
 
 type Props = {
@@ -8,25 +9,23 @@ type Props = {
 }
 export const Poster = ({ tvdbId, zoom }: Props) => (
   <Wrapper zoom={zoom}>
-    <Image src={images.poster.small(tvdbId)} />
+    <LazyLoadImage
+      width="100%"
+      height="auto"
+      defaultImage="https://www.placecage.com/g/185/273"
+      image={images.poster.small(tvdbId)}
+    />
   </Wrapper>
 )
 
-const Image = styled.img.attrs({
-  src: (props: { src?: string }) => props.src
-})`
-  width: 100%;
-`
 const Wrapper = styled.div`
   position: relative;
   cursor: pointer;
   ${(props: { zoom?: boolean }) =>
     props.zoom
       ? `transition: 0.3s ease-in-out;
-  &:hover {
-    -webkit-transform: scale(1.05);
-    -ms-transform: scale(1.05);
-    transform: scale(1.05);
-  }`
+         &:hover {
+          transform: scale(1.05);
+         }`
       : ''};
 `
