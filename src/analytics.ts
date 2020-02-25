230b2c8cf4b641eec206c35c5b5697fe7ac19b8e
tjoskar <kontakta@oskarkarlsson.nu>
2020-02-25 14:00:41 +0100

    🐛

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,6 +1,11 @@
+let cid = 0
+
 export function pageview(url: string) {
+  if (!cid) {
+    cid = (Math.random() * 100000000000000) | 0
+  }
   window.navigator.sendBeacon(
     'https://ncgtpliylg.execute-api.us-east-1.amazonaws.com/prod/hugin',
-    JSON.stringify({ type: 'pageview', cid: '555', url })
+    JSON.stringify({ type: 'pageview', cid, url })
   )
 }
