825fab2989d5f44401834dabf053b7a56ff8c908
tjoskar <kontakta@oskarkarlsson.nu>
2020-03-28 18:22:18 +0100

    🐛  Fix cid

--- src/analytics.ts
+++ src/analytics.ts
@@ -2,7 +2,7 @@ let cid = 0
 
 export function pageview(url: string) {
   if (!cid) {
-    cid = (Math.random() * 100000000000000) | 0
+    cid = (Math.random() * 100000) | 0
   }
   window.navigator.sendBeacon(
     'https://ncgtpliylg.execute-api.us-east-1.amazonaws.com/prod/hugin',
