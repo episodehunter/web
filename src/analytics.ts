a7e9c0f3c926141a4c3dc6bc6b1155e7c4570c46
tjoskar <kontakta@oskarkarlsson.nu>
2020-08-31 18:38:20 +0200

    🐛  wrap send beacon

--- src/analytics.ts
+++ src/analytics.ts
@@ -4,8 +4,10 @@ export function pageview(url: string) {
   if (!cid) {
     cid = (Math.random() * 100000) | 0
   }
-  window.navigator.sendBeacon(
-    'https://ncgtpliylg.execute-api.us-east-1.amazonaws.com/prod/hugin',
-    JSON.stringify({ type: 'pageview', cid, url })
-  )
+  try {
+    window.navigator.sendBeacon(
+      'https://ncgtpliylg.execute-api.us-east-1.amazonaws.com/prod/hugin',
+      JSON.stringify({ type: 'pageview', cid, url })
+    )
+  } catch (e) {}
 }
